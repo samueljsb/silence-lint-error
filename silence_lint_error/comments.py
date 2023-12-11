f73c69af5a898df238fea77739a00a3a973a6c68
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-12-11 17:10:03 +0000

    Allow error silencing comments to be added for any tool
    This will allow us to use these functions to add `lint-fixme` comments
    for `fixit`.

--- silence_lint_error/comments.py
+++ silence_lint_error/comments.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 import tokenize_rt
 
 
-def add_noqa_comments(src: str, lines: set[int], error_code: str) -> str:
-    """Add `noqa` comments to some code.
+def add_error_silencing_comments(
+        src: str, error_lines: set[int],
+        comment_type: str, error_code: str,
+) -> str:
+    """Add comments to some code to silence linting errors.
 
     Args:
-        src: The content of the module to add `nqa` comments to.
-        lines: The lines on which to add `noqa` coments.
+        src: The content of the module to add comments to.
+        error_lines: The lines on which to silence errors.
+        comment_type: The type of comment to add (e.g. `noqa` or `lint-fixme`)
         code: The error code to silence.
 
     Returns:
@@ -17,7 +21,7 @@ def add_noqa_comments(src: str, lines: set[int], error_code: str) -> str:
     tokens = tokenize_rt.src_to_tokens(src)
 
     for idx, token in tokenize_rt.reversed_enumerate(tokens):
-        if token.line not in lines:
+        if token.line not in error_lines:
             continue
         if not token.src.strip():
             continue
@@ -27,13 +31,29 @@ def add_noqa_comments(src: str, lines: set[int], error_code: str) -> str:
             tokens[idx] = tokens[idx]._replace(src=new_comment)
         else:
             tokens.insert(
-                idx+1, tokenize_rt.Token('COMMENT', f'# noqa: {error_code}'),
+                idx+1, tokenize_rt.Token(
+                    'COMMENT', f'# {comment_type}: {error_code}',
+                ),
             )
             tokens.insert(idx+1, tokenize_rt.Token('UNIMPORTANT_WS', '  '))
 
-        lines.remove(token.line)
+        error_lines.remove(token.line)
 
     return tokenize_rt.tokens_to_src(tokens)
+
+
+def add_noqa_comments(src: str, lines: set[int], error_code: str) -> str:
+    """Add `noqa` comments to some code.
+
+    Args:
+        src: The content of the module to add `noqa` comments to.
+        lines: The lines on which to add `noqa` coments.
+        code: The error code to silence.
+
+    Returns:
+        The content of the module with the additional comments added.
+    """
+    return add_error_silencing_comments(src, lines, 'noqa', error_code)
 
 
 def add_code_to_comment(comment: str, comment_type: str, code: str) -> str:
